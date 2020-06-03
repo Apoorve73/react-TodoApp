28657ace6573831f00f1a968a5a45eef8b99839b
Apoorve73 <apoorve73@gmail.com>
2020-06-03 23:50:05 +0530

    Updates

---
+++ precache-manifest.c02c4383f0260b7c80a01117b6fcaa85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84c8faebd0612eacee84c51d717762f5",
+    "url": "/react-TodoApp/index.html"
+  },
+  {
+    "revision": "5b8d3431c337430c66b5",
+    "url": "/react-TodoApp/static/css/main.43b9e605.chunk.css"
+  },
+  {
+    "revision": "d616f4b17a1582409cca",
+    "url": "/react-TodoApp/static/js/2.2e435aad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-TodoApp/static/js/2.2e435aad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b8d3431c337430c66b5",
+    "url": "/react-TodoApp/static/js/main.eb7e5702.chunk.js"
+  },
+  {
+    "revision": "0fa1d32b015f41b7e43e",
+    "url": "/react-TodoApp/static/js/runtime-main.ce9de6b3.js"
+  }
+]);
